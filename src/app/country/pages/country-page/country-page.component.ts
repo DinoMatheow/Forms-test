e54eb9c0869d1eaf4a2167645e1c1480f8bd81cc
Matheow <matius152014@gmail.com>
2025-04-22 19:36:21 -0500

    country service update

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,10 +1,57 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { JsonPipe } from '@angular/common';
+import { CountryList } from '../../interfaces/country.interfaces';
+import { switchMap, tap } from 'rxjs';
 @Component({
   selector: 'app-country-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<CountryList[]>([]);
+  borders = signal<CountryList[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChange = effect((onCleanup)=> {
+    const regionSubscription = this.onRegionChanged();
+    onCleanup(()=> {
+      regionSubscription.unsubscribe();
+    });
+  });
+
+  onRegionChanged(){
+    return this.myForm.get('region')!.valueChanges.pipe(
+      tap(()=> this.myForm.get('country')!.setValue('')),
+      tap(()=> this.myForm.get('border')!.setValue('')),
+      tap(()=> {
+          this.borders.set([]);
+          this.countriesByRegion.set([]);
+      }),
+      switchMap((region)=> this.countryService.getCountryByRegion(region!))
+    )
+    .subscribe((countries)=> {
+      this.countriesByRegion.set(countries);
+    });
+  }
+
+}
+
+
+
+
+
---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,48 @@
+  import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CountryList } from '../interfaces/country.interfaces';
+import { Observable, of } from 'rxjs';
+  @Injectable({providedIn: 'root'})
+  export class CountryService {
+
+    private baseUrl = 'https://restcountries.com/v3.1';
+    http = inject(HttpClient);
+
+
+    private _region = [
+      'Africa',
+      'Americas',
+      'Asia',
+      'Europe',
+      'Oceania'
+    ]
+
+    get regions(): string[]{
+      return [...this._region];
+    }
+
+    getCountryByRegion(region: string): Observable<CountryList[]>
+    {
+      if(!region) return of([]);
+
+      console.log(region);
+
+      const url = `${this.baseUrl}/region/${region}?fields=name,borders,cca3`;
+      return this.http.get<CountryList[]>(url);
+    }
+
+    getCountryByAlphaCode(code: string): Observable<CountryList>
+    {
+
+      const url = `${this.baseUrl}/alpha/${code}?fields=name,borders,cca3`;
+      return this.http.get<CountryList>(url);
+    }
+
+    getCountryByBorder(borders: string[]){
+
+    }
+
+
+
+
+  }
